1a8a3b0f6d1507d583220aff8f56a8fa3d92a81b
Hithesh B <hitesh99017@gmail.com>
2024-12-28 23:59:29 +0530

    worked om backend

--- Backend/index.js
+++ Backend/index.js
@@ -6,19 +6,20 @@ import connectDB from "./utils/db.js";
 const app = express();
 
 dotenv.config();
+connectDB();
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173", 
     credentials: true,
 };
 app.use(cors(corsOptions)); 
 
 
-app.listen(5000, () => {
-    connectDB();
-    console.log("Server is running on port 5000");
+app.listen(3000, () => {
+     
+    console.log("Server is running on port 3000");
 });
